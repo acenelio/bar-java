3ad0d0a7d738c82c3a0ad03960036562d19957e6
Nelio Alves <acenelio@gmail.com>
2020-05-04 12:32:01 -0300

    Relatorio gerado

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -12,6 +12,9 @@ public class Programa {
 
 		char sexo;
 		int cervejas, refri, espetos;
+		double consumo, couvert, ingresso, total;
+		
+		// 1 LEITURA DOS DADOS
 		
 		System.out.print("Sexo: ");
 		sexo = sc.next().charAt(0);
@@ -21,6 +24,41 @@ public class Programa {
 		refri = sc.nextInt();
 		System.out.print("Quantidade de espetinhos: ");
 		espetos = sc.nextInt();
+		
+		// 2 CALCULOS
+		
+		if (sexo == 'F') {
+			ingresso = 8.0;
+		}
+		else {
+			ingresso = 10.0;
+		}
+		
+		consumo = cervejas * 5.0 + refri * 3.0 + espetos * 7.0;
+		
+		if (consumo > 30) {
+			couvert = 0.0;
+		}
+		else {
+			couvert = 4.0;
+		}
+		
+		total = ingresso + consumo + couvert;
+
+		// 3 RELATORIO
+		
+		System.out.println();
+		System.out.println("RELATÓRIO:");
+		System.out.printf("Consumo = R$ %.2f%n", consumo);
+		if (couvert == 0.0) {
+			System.out.println("Isento de Couvert");
+		}
+		else {
+			System.out.printf("Couvert = R$ %.2f%n", couvert);
+		}
+		System.out.printf("Ingresso = R$ %.2f%n", ingresso);
+		System.out.println();
+		System.out.printf("Valor a pagar = R$ %.2f%n", total);
 		
 		sc.close();
 	}
